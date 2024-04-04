c8f9cdcd853d02e7fa35c8c75935f63ce5a6354e
unknown <llqrw55@naver.com>
2024-04-05 01:07:30 +0900

    Feat: placeholder 수정

--- co-kkiri/src/components/commons/ReactQuill/index.tsx
+++ co-kkiri/src/components/commons/ReactQuill/index.tsx
@@ -87,7 +87,7 @@ export default function QuillEditor({ onChange, value }: { onChange: (value: str
           formats={formats}
           onChange={onChange}
           className="custom-quill-editor"
-          placeholder="2000자 내로 입력해주세요."
+          placeholder="프로젝트를 소개해주세요!"
         />
       </ReactQuillWrapper>
     </>

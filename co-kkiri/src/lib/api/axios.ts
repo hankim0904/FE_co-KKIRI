ef82b1e97129c4d44e40c1f46db78326eca61d16
김하은 <han.kim0904@gmail.com>
2025-03-20 19:46:33 +0900

    Chore: cors 문제 해결

--- co-kkiri/src/lib/api/axios.ts
+++ co-kkiri/src/lib/api/axios.ts
@@ -10,7 +10,7 @@ export interface ApiRequestResponse<T> {
 export const BASE_URL = import.meta.env.VITE_BASE_API_URL;
 
 export const axiosInstance = axios.create({
-  baseURL: BASE_URL,
+  baseURL: "/api",
   withCredentials: true,
 });
 
